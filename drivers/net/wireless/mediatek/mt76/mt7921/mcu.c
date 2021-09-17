7ec127d704b63203a8e43c3e74ee8253b87ef1cc
Sean Wang <sean.wang@mediatek.com>
2021-09-18 07:59:24 +0800

    FROMLIST: mt76: mt7921: make all event parser reusable between mt7921s and mt7921e
    The longer event such as the event for mcu_get_nic_capability would hold
    the data in paged fragment skb for the SDIO device so we turn the skb to
    be linearized skb before accessing it to reuse the same event parser
    betweem mt7921s and mt7921e.
    
    Tested-by: Deren Wu <deren.wu@mediatek.com>
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    (am from https://patchwork.kernel.org/patch/12503303/)
    (also found at https://lore.kernel.org/r/a6aebd446a9aa7902397a7e7b87fe427076fc9df.1631918993.git.objelf@gmail.com)
    
    BUG=b:178754244
    TEST=build on cherry
    
    Signed-off-by: Sean Wang <objelf@gmail.com>
    Change-Id: Ibfed456b96564eb4ba47c780d8bb83f1afa1e0e1

--- drivers/net/wireless/mediatek/mt76/mt7921/mcu.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/mcu.c
@@ -456,7 +456,14 @@ mt7921_mcu_rx_unsolicited_event(struct mt7921_dev *dev, struct sk_buff *skb)
 
 void mt7921_mcu_rx_event(struct mt7921_dev *dev, struct sk_buff *skb)
 {
-	struct mt7921_mcu_rxd *rxd = (struct mt7921_mcu_rxd *)skb->data;
+	struct mt7921_mcu_rxd *rxd;
+	int ret;
+
+	ret = skb_linearize(skb);
+	if (ret)
+		return;
+
+	rxd = (struct mt7921_mcu_rxd *)skb->data;
 
 	if (rxd->eid == 0x6) {
 		mt76_mcu_rx_event(&dev->mt76, skb);
